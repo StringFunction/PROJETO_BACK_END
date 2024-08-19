8724b28f6aef92c22950f6ce6b31b0655013d2e1
FRANCISCO CLÉCIO LIMA DA SILVA <FRANCISCO0794@DIGITALCOLLEGE.LOCAL>
2024-08-19 08:12:34 -0300

    resolvendo conflito

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const http = require('http')
 const app = require('./src/app')
 require('./src/routes/routes_usuario')
-const port = process.env.PORTA || 10000
+const port = process.env.PORTA || 1000
 require('./src/config/database')
 
 const server = http.createServer(app)
